41411aa7da2ca560642503d1db1fa410e525ff1c
Kenny Miller <KENNETHMILLER@boisestate.edu>
2025-12-07 17:05:29 -0700

    Wip adding in authentication and main home page

--- src/BasicFinance.Client/src/app/app.config.ts
+++ src/BasicFinance.Client/src/app/app.config.ts
@@ -1,11 +1,13 @@
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { includeBearerTokenInterceptor, provideKeycloak } from 'keycloak-angular';
 import { routes } from './app.routes';
+import { keycloakProvider } from './keycloak.config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,5 +15,7 @@ export const appConfig: ApplicationConfig = {
     provideZonelessChangeDetection(),
     provideRouter(routes),
     provideHttpClient(),
+    provideKeycloak(keycloakProvider),
+    provideHttpClient(withInterceptors([includeBearerTokenInterceptor])),
   ],
 };
---
+++ src/BasicFinance.Client/src/app/keycloak.config.ts
@@ -0,0 +1,40 @@
+import {
+  AutoRefreshTokenService,
+  createInterceptorCondition,
+  INCLUDE_BEARER_TOKEN_INTERCEPTOR_CONFIG,
+  IncludeBearerTokenCondition,
+  ProvideKeycloakOptions,
+  UserActivityService,
+  withAutoRefreshToken,
+} from 'keycloak-angular';
+
+const localhostCondition = createInterceptorCondition<IncludeBearerTokenCondition>({
+  urlPattern: /^(https:\/\/localhost:7119)(\/.*)?$/i, // Match URLs starting with http://localhost:8181
+});
+
+export const keycloakProvider: ProvideKeycloakOptions = {
+  config: {
+    url: 'http://localhost:8080',
+    realm: 'basic-hub',
+    clientId: 'basic-finance-public',
+  },
+  initOptions: {
+    onLoad: 'check-sso',
+    silentCheckSsoRedirectUri: `${window.location.origin}/silent-check-sso.html`,
+    redirectUri: window.location.origin + '/',
+  },
+  features: [
+    withAutoRefreshToken({
+      onInactivityTimeout: 'logout',
+      sessionTimeout: 1000,
+    }),
+  ],
+  providers: [
+    AutoRefreshTokenService,
+    UserActivityService,
+    {
+      provide: INCLUDE_BEARER_TOKEN_INTERCEPTOR_CONFIG,
+      useValue: [localhostCondition],
+    },
+  ],
+};
