7208b6c23e3f5cd55135d16df68871e723d448db
Kenny Miller <kennymilarhol@gmail.com>
2025-12-13 18:26:30 -0700

    Removed comments

--- src/BasicFinance.Client/src/app/core/auth/auth.config.ts
+++ src/BasicFinance.Client/src/app/core/auth/auth.config.ts
@@ -12,28 +12,3 @@ export const authConfig: AuthConfig = {
   timeoutFactor: 0.01,
   checkOrigin: true,
 };
-
-//   config: {
-//     url: 'http://localhost:8080',
-//     realm: 'basic-hub',
-//     clientId: 'basic-finance-public',
-//   },
-//   initOptions: {
-//     onLoad: 'check-sso',
-//     silentCheckSsoRedirectUri: `${window.location.origin}/silent-check-sso.html`,
-//     redirectUri: window.location.origin + '/',
-//   },
-//   features: [
-//     withAutoRefreshToken({
-//       onInactivityTimeout: 'logout',
-//       sessionTimeout: 1000,
-//     }),
-//   ],
-//   providers: [
-//     AutoRefreshTokenService,
-//     UserActivityService,
-//     {
-//       provide: INCLUDE_BEARER_TOKEN_INTERCEPTOR_CONFIG,
-//       useValue: [localhostCondition],
-//     },
-//   ],
